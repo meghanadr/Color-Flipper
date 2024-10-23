aaf9f76ef72fbb67d1a71e8acd703bcd36175ecd
meghanadr <119675901+meghanadr@users.noreply.github.com>
2024-10-24 00:36:42 +0530

    Update app.js

--- Counter/app.js
+++ Counter/app.js
@@ -1,23 +1,26 @@
-const value= document.querySelector("#value");
-const btns=document.querySelectorAll(".btn")
+let count =0;// we set intial valve to count=0
+const value = document.querySelector("#value");//we are fetching span value by queryselector
+const btns =  document.querySelectorAll(".btn");// button value in container by queryselectorAll
 
-btns.forEach(function(btn){
-    btn.addEventListener("clicks",function(e){
-        const styles=e.currentTarget.classList;
-        if(styles.contains="decrease"){
+
+btns.forEach(function(btn){  // we are writing btn function whenever user click on btn for we are selecting which button by foreach
+    btn.addEventListener("click",function(e){// calling eventlistenser
+        const styles=e.currentTarget.classList;// figureing out which class is calling in classlist 
+        console.log("stylessss",styles);
+        if(styles.contains("decrease")){
             count--;
-        }else if(styles.contains="increase"){
+        }else if(styles.contains("Increase")){
             count++;
         }else{
             count=0;
         }
         if(count>0){
-            value.styles.color="green"
-        }else if(count<0){
-            value.styles.color="red"
-        }else if(count===0){
-            value.styles.color="#222"
-        }
+            value.style.color="green";
+        }if(count<0){
+            value.style.color="red";
+        } if(count===0){
+            value.style.color="#222";
+         }
         value.textContent=count;
     });
 });
